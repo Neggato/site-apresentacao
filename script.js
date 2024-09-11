ea3eae44846c2ad4270448fc634e080956500dcf
jotave <jvfelix86@gmail.com>
2024-09-11 00:37:57 -0300

    FINAL

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+
+    slideIndex += n;
+
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
